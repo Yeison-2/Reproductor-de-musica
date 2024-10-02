85a80848310f9a1e6acce0edd89abb6c091412c8
AndresMartinex <afelipemartinez@jdc.edu.co>
2024-10-02 15:10:53 -0500

    re-estructuracion del controlador

--- Reproductor_Music_Project/src/main/java/controlador/controlador_playList.java
+++ Reproductor_Music_Project/src/main/java/controlador/controlador_playList.java
@@ -5,6 +5,7 @@
  */
 package controlador;
 
+import java.util.Scanner;
 import modelo.modelo_playList;
 import vista.vista_playList;
 
@@ -14,48 +15,70 @@ import vista.vista_playList;
  */
 public class controlador_playList {
 
-    private modelo_playList modelo;
-    private vista_playList view;
+   private modelo_playList playlist;
+   private vista_playList view;
 
-    public controlador_playList(modelo_playList modelo, vista_playList view) {
-        this.modelo = modelo;
+    public controlador_playList(modelo_playList playlist, vista_playList view) {
+        this.playlist = playlist;
         this.view = view;
     }
 
-    public void iniciar() {
-        boolean continuar = true;
-        int id=0;// toca crear metodo para crear id
-        while (continuar) {
-            int opcion = view.mostrarMenu();
-
-            switch (opcion) {
-                
-                
-            }
-            /*toca mirar como se puede manejar esta parte*/
-            
-            
-//                case 1:
-//                    int numero = view.eleccion();
-//                    modelo.agregarCancion(numero);
-//                    view.mostrarMensaje("Cancion agregada correctamente");
-//                    break;
-//                case 2:
-//                    int quitar = modelo.eliminarCancion(id);
-//                    view.mostrarMensaje("Cancion elminada de la playList" + quitar);
-//                    break;
-//
-//                case 3:
-//                    modelo.mostrarPlayList();
-//                    break;
-//                case 4:
-//                    continuar = false;
-//                    view.mostrarMensaje("saliendo.......");
-//                    break;
-//                default:
-//                    view.mostrarMensaje("opcion invalida");
-//                    break;
-//            }
+    
+    public void ejecutarAccion(int opcion) {
+        switch (opcion) {
+            case 1:
+                agregarCancion();
+                break;
+            case 2:
+                eliminarCancion();
+                break;
+            case 3:
+                mostrarPlayList();
+                break;
+            case 4:
+                salir();
+                break;
+            default:
+                System.out.println("Opción no válida");
+                break;
         }
     }
-}
+
+    private void agregarCancion() {
+        System.out.println("Ingrese el título de la canción:");
+        String titulo = leerCadena();
+        System.out.println("Ingrese el artista de la canción:");
+        String artista = leerCadena();
+        playlist.agregarCancion(titulo, artista);
+    }
+
+    private void eliminarCancion() {
+        System.out.println("Ingrese el ID de la canción que desea eliminar:");
+        int id = leerEntero();
+        if (playlist.eliminarCancion(id)) {
+            System.out.println("Canción eliminada con éxito");
+        } else {
+            System.out.println("No se encontró la canción con el ID ingresado");
+        }
+    }
+
+    private void mostrarPlayList() {
+        playlist.mostrarPlayList();
+    }
+
+    private void salir() {
+        System.out.println("Saliendo del programa...");
+        System.exit(0);
+    }
+
+    private String leerCadena() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+
+    private int leerEntero() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextInt();
+    }
+    }
+
