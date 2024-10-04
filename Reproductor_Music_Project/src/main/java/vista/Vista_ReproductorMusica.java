478e36ab413f4ee593908beaf14207a5b1c5fe47
YeisonRS <yeisonstivenromero@gmail.com>
2024-10-04 17:28:36 -0500

    se comenzo a hacer el enlace entre la vista y el controlador

--- Reproductor_Music_Project/src/main/java/vista/Vista_ReproductorMusica.java
+++ Reproductor_Music_Project/src/main/java/vista/Vista_ReproductorMusica.java
@@ -7,8 +7,10 @@ package vista;
 import com.formdev.flatlaf.FlatLightLaf;
 import com.formdev.flatlaf.intellijthemes.FlatCarbonIJTheme;
 import java.awt.Dimension;
+import java.awt.event.ActionListener;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
 /**
@@ -24,9 +26,8 @@ public class Vista_ReproductorMusica extends javax.swing.JFrame {
         this.setTitle("Reproductor de musica");
         this.setSize(new Dimension(1021, 650));
         configurarPlaceholder(JTF_BuscaCanciones, "Buscar Cancion");
-        
-    }
 
+    }
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
@@ -34,18 +35,19 @@ public class Vista_ReproductorMusica extends javax.swing.JFrame {
 
         jPanel4 = new javax.swing.JPanel();
         jPanel1 = new javax.swing.JPanel();
-        jSlider1 = new javax.swing.JSlider();
-        jButton4 = new javax.swing.JButton();
-        jButton5 = new javax.swing.JButton();
-        jButton6 = new javax.swing.JButton();
+        JL_progresoMusica = new javax.swing.JSlider();
+        JB_play = new javax.swing.JButton();
+        JB_anterior = new javax.swing.JButton();
+        JB_siguiente = new javax.swing.JButton();
         jTabbedPane1 = new javax.swing.JTabbedPane();
         jPanel3 = new javax.swing.JPanel();
         jScrollPane1 = new javax.swing.JScrollPane();
-        jTable1 = new javax.swing.JTable();
-        jLabel4 = new javax.swing.JLabel();
+        JT_tablaCancioArtista = new javax.swing.JTable();
+        JL_tituloCanciones = new javax.swing.JLabel();
         JTF_BuscaCanciones = new javax.swing.JTextField();
-        jButton3 = new javax.swing.JButton();
-        jButton1 = new javax.swing.JButton();
+        JB_buscarCancion = new javax.swing.JButton();
+        JB_añadirCancion = new javax.swing.JButton();
+        JB_eliminarCancion = new javax.swing.JButton();
         jPanel5 = new javax.swing.JPanel();
 
         javax.swing.GroupLayout jPanel4Layout = new javax.swing.GroupLayout(jPanel4);
@@ -61,47 +63,66 @@ public class Vista_ReproductorMusica extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jPanel1.setBackground(new java.awt.Color(212, 217, 217));
+        jPanel1.setBackground(new java.awt.Color(0, 0, 0));
 
-        jButton4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/Play.png"))); // NOI18N
-        jButton4.setBorderPainted(false);
-        jButton4.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        JL_progresoMusica.setBackground(new java.awt.Color(255, 255, 255));
 
-        jButton5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/anterior.png"))); // NOI18N
-        jButton5.setBorderPainted(false);
-        jButton5.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        JB_play.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/Play.png"))); // NOI18N
+        JB_play.setBorderPainted(false);
+        JB_play.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        JB_play.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                JB_playActionPerformed(evt);
+            }
+        });
 
-        jButton6.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/siguiente.png"))); // NOI18N
-        jButton6.setBorderPainted(false);
-        jButton6.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        JB_anterior.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/anterior.png"))); // NOI18N
+        JB_anterior.setBorderPainted(false);
+        JB_anterior.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        JB_anterior.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                JB_anteriorActionPerformed(evt);
+            }
+        });
+
+        JB_siguiente.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/siguiente.png"))); // NOI18N
+        JB_siguiente.setBorderPainted(false);
+        JB_siguiente.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        JB_siguiente.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                JB_siguienteActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jSlider1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(JL_progresoMusica, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGap(405, 405, 405)
-                .addComponent(jButton5)
+                .addComponent(JB_anterior)
                 .addGap(53, 53, 53)
-                .addComponent(jButton4)
+                .addComponent(JB_play)
                 .addGap(48, 48, 48)
-                .addComponent(jButton6)
+                .addComponent(JB_siguiente)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addComponent(jSlider1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(JL_progresoMusica, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(23, 23, 23)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jButton4)
-                    .addComponent(jButton5)
-                    .addComponent(jButton6))
+                    .addComponent(JB_play)
+                    .addComponent(JB_anterior)
+                    .addComponent(JB_siguiente))
                 .addGap(0, 42, Short.MAX_VALUE))
         );
 
-        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+        jPanel3.setBackground(new java.awt.Color(255, 255, 255));
+
+        JT_tablaCancioArtista.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
                 {null, null},
                 {null, null},
@@ -121,11 +142,11 @@ public class Vista_ReproductorMusica extends javax.swing.JFrame {
                 return canEdit [columnIndex];
             }
         });
-        jTable1.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
-        jScrollPane1.setViewportView(jTable1);
+        JT_tablaCancioArtista.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        jScrollPane1.setViewportView(JT_tablaCancioArtista);
 
-        jLabel4.setFont(new java.awt.Font("Segoe UI", 0, 24)); // NOI18N
-        jLabel4.setText("Canciones");
+        JL_tituloCanciones.setFont(new java.awt.Font("Segoe UI", 0, 24)); // NOI18N
+        JL_tituloCanciones.setText("Canciones");
 
         JTF_BuscaCanciones.setText("Buscar Cancion");
         JTF_BuscaCanciones.addActionListener(new java.awt.event.ActionListener() {
@@ -134,47 +155,79 @@ public class Vista_ReproductorMusica extends javax.swing.JFrame {
             }
         });
 
-        jButton3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/SearchBuscar.png"))); // NOI18N
-        jButton3.setText("Buscar");
-        jButton3.setBorderPainted(false);
-        jButton3.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        JB_buscarCancion.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/SearchBuscar.png"))); // NOI18N
+        JB_buscarCancion.setText("Buscar");
+        JB_buscarCancion.setBorderPainted(false);
+        JB_buscarCancion.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        JB_buscarCancion.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                JB_buscarCancionActionPerformed(evt);
+            }
+        });
 
-        jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/SymbolsAddSuma.png"))); // NOI18N
-        jButton1.setText("Añadir ");
-        jButton1.setBorderPainted(false);
-        jButton1.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        JB_añadirCancion.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/SymbolsAddSuma.png"))); // NOI18N
+        JB_añadirCancion.setText("Añadir ");
+        JB_añadirCancion.setBorderPainted(false);
+        JB_añadirCancion.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        JB_añadirCancion.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                JB_añadirCancionActionPerformed(evt);
+            }
+        });
+
+        JB_eliminarCancion.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/eliminar.png"))); // NOI18N
+        JB_eliminarCancion.setText("Eliminar");
+        JB_eliminarCancion.setBorderPainted(false);
+        JB_eliminarCancion.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        JB_eliminarCancion.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                JB_eliminarCancionActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
         jPanel3.setLayout(jPanel3Layout);
         jPanel3Layout.setHorizontalGroup(
             jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel3Layout.createSequentialGroup()
-                .addGap(33, 33, 33)
-                .addComponent(jLabel4)
-                .addGap(40, 40, 40)
-                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(jButton1)
-                    .addComponent(JTF_BuscaCanciones, javax.swing.GroupLayout.PREFERRED_SIZE, 434, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(32, 32, 32)
-                .addComponent(jButton3)
-                .addContainerGap(284, Short.MAX_VALUE))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel3Layout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 434, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(293, 293, 293))
+                .addComponent(JB_añadirCancion)
+                .addGap(307, 307, 307))
+            .addGroup(jPanel3Layout.createSequentialGroup()
+                .addGap(33, 33, 33)
+                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(jPanel3Layout.createSequentialGroup()
+                        .addComponent(JB_eliminarCancion)
+                        .addGap(135, 135, 135)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 434, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel3Layout.createSequentialGroup()
+                        .addComponent(JL_tituloCanciones)
+                        .addGap(45, 45, 45)
+                        .addComponent(JB_buscarCancion)
+                        .addGap(18, 18, 18)
+                        .addComponent(JTF_BuscaCanciones, javax.swing.GroupLayout.PREFERRED_SIZE, 434, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(293, Short.MAX_VALUE))
         );
         jPanel3Layout.setVerticalGroup(
             jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel3Layout.createSequentialGroup()
                 .addContainerGap(37, Short.MAX_VALUE)
-                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel4)
-                    .addComponent(JTF_BuscaCanciones, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButton3))
-                .addGap(13, 13, 13)
-                .addComponent(jButton1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 357, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel3Layout.createSequentialGroup()
+                        .addComponent(JL_tituloCanciones)
+                        .addGap(49, 49, 49))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel3Layout.createSequentialGroup()
+                        .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(JTF_BuscaCanciones, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(JB_buscarCancion))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(JB_añadirCancion)
+                        .addGap(6, 6, 6)))
+                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 357, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(jPanel3Layout.createSequentialGroup()
+                        .addGap(27, 27, 27)
+                        .addComponent(JB_eliminarCancion)))
                 .addGap(12, 12, 12))
         );
 
@@ -212,12 +265,84 @@ public class Vista_ReproductorMusica extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void JTF_BuscaCancionesActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_JTF_BuscaCancionesActionPerformed
-        
+
     }//GEN-LAST:event_JTF_BuscaCancionesActionPerformed
 
+    private void JB_añadirCancionActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_JB_añadirCancionActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_JB_añadirCancionActionPerformed
+
+    private void JB_eliminarCancionActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_JB_eliminarCancionActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_JB_eliminarCancionActionPerformed
+
+    private void JB_buscarCancionActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_JB_buscarCancionActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_JB_buscarCancionActionPerformed
+
+    private void JB_playActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_JB_playActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_JB_playActionPerformed
+
+    private void JB_anteriorActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_JB_anteriorActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_JB_anteriorActionPerformed
+
+    private void JB_siguienteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_JB_siguienteActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_JB_siguienteActionPerformed
+
+    //metodos para enviar todo al controlador
+    /**
+     * metodo que retorna la cancion a buscar con String para usurla en el
+     * controlador
+     *
+     * @return el dato que esta en el JTF_buscarCancion
+     */
+    public String getpedirCancionString() {
+        return JTF_BuscaCanciones.getText();
+    }
+
+    //metodo para agregar el nombre y el autor a la table
+    /**
+     * Metodo para mostra notificaciones de errores como por ejemplo dato no
+     * valido
+     *
+     * @param error String
+     */
+    public void verError(String error) {
+        JOptionPane.showMessageDialog(this, error, "Erro", JOptionPane.ERROR_MESSAGE);
+    }
+
+    /**
+     * Metodo que retorna las interacciones con los botones y le asigna un
+     * comando exclusivo a cada uno para que puedan se utillizados por ejemplo
+     * en el controlador
+     *
+     * @param listener 
+     */
+    public void addListener(ActionListener listener) {
+        JB_play.setActionCommand("play");//le asigno un comando especifico para llamarlo
+        JB_play.addActionListener(listener);
+
+        JB_siguiente.setActionCommand("siguiente");
+        JB_siguiente.addActionListener(listener);
+
+        JB_anterior.setActionCommand("anterior");
+        JB_anterior.addActionListener(listener);
+
+        //eliminar , añadir, buscar
+        JB_eliminarCancion.setActionCommand("eliminar");
+        JB_eliminarCancion.addActionListener(listener);
+
+        JB_añadirCancion.setActionCommand("añadir");
+        JB_añadirCancion.addActionListener(listener);
+
+        JB_buscarCancion.setActionCommand("buscar");
+        JB_buscarCancion.addActionListener(listener);
+    }
+
     public static void main(String args[]) {
-        
-        
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
@@ -253,20 +378,21 @@ public class Vista_ReproductorMusica extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton JB_anterior;
+    private javax.swing.JButton JB_añadirCancion;
+    private javax.swing.JButton JB_buscarCancion;
+    private javax.swing.JButton JB_eliminarCancion;
+    private javax.swing.JButton JB_play;
+    private javax.swing.JButton JB_siguiente;
+    private javax.swing.JSlider JL_progresoMusica;
+    private javax.swing.JLabel JL_tituloCanciones;
     private javax.swing.JTextField JTF_BuscaCanciones;
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton3;
-    private javax.swing.JButton jButton4;
-    private javax.swing.JButton jButton5;
-    private javax.swing.JButton jButton6;
-    private javax.swing.JLabel jLabel4;
+    private javax.swing.JTable JT_tablaCancioArtista;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel3;
     private javax.swing.JPanel jPanel4;
     private javax.swing.JPanel jPanel5;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JSlider jSlider1;
     private javax.swing.JTabbedPane jTabbedPane1;
-    private javax.swing.JTable jTable1;
     // End of variables declaration//GEN-END:variables
 }
